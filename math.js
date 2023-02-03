afd58c571257813dbb9c9ca7e822bd08f64c8183
roman454647 <roman.pidkostelnii@gmail.com>
2023-02-03 15:33:44 +0200

    inial commit

---
+++ math.js
@@ -0,0 +1,17 @@
+const sum = (a, b) => {
+    return a + b
+}
+
+const minus = (a, b) => {
+    return a - b
+}
+
+const multiply = (a, b) => {
+    return a * b
+}
+
+const divide = (a, b) => {
+    return a / b
+}
+
+module.exports = {sum, divide, multiply, minus}
